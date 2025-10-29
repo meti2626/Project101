41c881cf8e82b2d80a386c8a082bdfb0e0bbe36a
meti2626 <metseboka@gmail.com>
2025-10-29 19:54:39 +0300

    oct29

---
+++ ISearchEngine/script.js
@@ -0,0 +1,11 @@
+
+const searchForm = document.getElmentById("search-form");
+const searchBox = document.getElmentById("search-box");
+const searchResult = document.getElmentById("search-result");
+const  showMoreBtn= document.getElmentById("show-more-btn");
+
+
+
+
+//DwyNUOqFBk8npNQ0NOD9IZXhK1Vfw9PE6WcxleksTWY
+https://api.unsplash.com/search/photos
