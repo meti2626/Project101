6fda049e1b9835488f297973173901ddd9502354
meti2626 <metseboka@gmail.com>
2025-11-02 20:06:58 +0300

    nov 2-3

--- SelfDrivingCar/car.js
+++ SelfDrivingCar/car.js
@@ -1,13 +1,15 @@
 
 class Car{
+
+    // x and y  -- tell where your car starts (its position on the road).
     constructor(x ,y ,width ,height){
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         
-         this.speed = 0;
-         this.acceleration = 0.2;
+         this.speed = 0; //how fast it’s going (start at 0
+         this.acceleration = 0.2;//how much it speeds up when you press the gas pedal. 
          this.maxSpeed = 3;
          this.friction = 0.05;
           
@@ -30,6 +32,28 @@ class Car{
            if (this.speed < -this.maxSpeed/2){
                 this.speed = -this.maxSpeed/2;
            }
+
+           if(this.speed >0)
+           {
+             this.speed -= this.friction;
+           }
+            
+           if(this.speed < 0){
+                this.speed += this.friction;
+           }
+
+           if(Math.abs(this.speed) < this.friction){
+               this.speed = 0;
+           }
+
+        if (this.controls.left){
+            this.x -= 2;
+        }
+
+        if (this.controls.right){
+            this.x += 2;
+        }
+
            this.y-= this.speed;
            
         }
